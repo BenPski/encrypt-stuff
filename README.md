3c15609013d05d895d53c8c8291620fe70db0e25
Ben Pski <ben@pepski.com>
2024-08-15 23:18:19 -0500

    some documentation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# encrypt-stuff
+# Encrypt Stuff
+
+Encrypt your serializable data.
+
+Choose your asymmetric encryption scheme and then convert your serialized data
+to an encrypted and serialized form.
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+/// Encrypt serializable data
+/// Choose the asymmetric encryption scheme you'd like to use and encrypt your
+/// serializable data.
+///
 pub mod encryption;
 pub mod error;
 pub mod schemes;
